573e55ccc195837b20341854071201068aada510
m.sredzinski94@gmail.com <m.sredzinski94@gmail.com>
2023-05-02 22:17:15 +0200

    Better hello widget work in progress

---
+++ BestPDemoSite/Components/PageBuilder/Widgets/BetterHelloWidget/BetterHelloWidgetProperties.cs
@@ -0,0 +1,12 @@
+﻿using Kentico.Forms.Web.Mvc;
+using Kentico.PageBuilder.Web.Mvc;
+using System.Reflection.Emit;
+
+namespace BestPDemoSite.Components.PageBuilder.Widgets.HelloWidget
+{
+    public class BetterHelloWidgetProperties : IWidgetProperties
+    {
+        [EditingComponent(TextInputComponent.IDENTIFIER, Order = 0, Label = "Welcome text")]
+        public string WelcomeText { get; set; } = "Hello"; 
+    }
+}
---
+++ BestPDemoSite/Components/PageBuilder/Widgets/BetterHelloWidget/BetterHelloWidgetViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using BestPDemoSite.Components.PageBuilder.Widgets.HelloWidget;
+using Kentico.PageBuilder.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace BestPDemoSite.Components.PageBuilder.Widgets.BetterHelloWidget
+{
+    public class BetterHelloWidgetViewComponent : ViewComponent
+    {
+        public async Task InvokeAsync(ComponentViewModel<BetterHelloWidgetProperties> properties)
+        {
+            properties.Properties.WelcomeText += "Mordeczki";
+        }
+    }
+}
